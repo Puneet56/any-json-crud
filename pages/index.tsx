f58bb3a6637e9a99b7a51ca1794e299594fc890f
Puneet Dhiman <puneet.dhiman@synlabs.io>
2022-11-11 01:07:44 +0530

    Initial Table

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,87 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import axios from "axios";
+import { GetServerSideProps } from "next";
+import Head from "next/head";
+import { Fragment } from "react";
 
-const Home: NextPage = () => {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
-    </div>
-  )
+interface User {
+	firstName: string;
+	lastName: string;
+	favouriteWord: string;
+	id: string;
 }
 
-export default Home
+const getKeys = (data: any[]) => {
+	let obj = data[0];
+
+	if (!obj) return [];
+
+	return Object.keys(obj);
+};
+
+const getHeaders = (data: string[]) =>
+	data.map((key) => {
+		let header = key.split(/(?=[A-Z])/).join(" ");
+		return header.charAt(0).toUpperCase() + header.slice(1);
+	});
+
+const Home = <T extends {}>({ data = [] }: { data: T[] }) => {
+	let keys = getKeys(data);
+	const headers = getHeaders(keys);
+
+	return (
+		<div className="flex min-h-screen bg-gray-800 flex-col items-center justify-start py-2">
+			<Head>
+				<title>Any JSON CRUD</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
+			<div className="container h-full flex flex-col items-center justify-start border">
+				<h1 className="text-4xl text-white font-bold">Any JSON CRUD</h1>
+				<div
+					style={{
+						gridTemplateColumns: `repeat(${keys.length}, minmax(200px, 1fr))`,
+					}}
+					className="grid text-white w-full border h-full overflow-auto"
+				>
+					{headers.map((header, i) => (
+						<p key={i} className="font-bold border text-center">
+							{header}
+						</p>
+					))}
+
+					{data.map((item, i) => (
+						<Fragment key={i}>
+							{keys.map((key, j) => (
+								<p key={j} className="border text-center">
+									{/* @ts-ignore   */}
+									{item[key]}
+									{/* TODO:fix this */}
+								</p>
+							))}
+						</Fragment>
+					))}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Home;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+	const API_URL = "https://622386e63af069a0f9a47530.mockapi.io/api";
+	let users = [];
+
+	try {
+		const data = await axios.get(`${API_URL}/users`);
+		users = data.data;
+	} catch (error) {
+		console.log(error);
+	}
+
+	return {
+		props: {
+			data: JSON.parse(JSON.stringify(users)),
+		},
+	};
+};
