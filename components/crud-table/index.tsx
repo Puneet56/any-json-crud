26d535bf9eb848c2437d84e72029ead2cbb2b89a
Puneet Dhiman <puneet.dhiman@synlabs.io>
2022-12-21 11:46:23 +0530

    Added prettierrc

--- components/crud-table/index.tsx
+++ components/crud-table/index.tsx
@@ -1,61 +1,62 @@
-import { Fragment } from "react";
-import { AiFillEdit, AiFillDelete } from "react-icons/ai"
+import { Fragment } from 'react';
+import { AiFillDelete, AiFillEdit } from 'react-icons/ai';
 
 const getKeys = (data: any[]) => {
-  let obj = data[0];
+	let obj = data[0];
 
-  if (!obj) return [];
+	if (!obj) return [];
 
-  return Object.keys(obj).filter((item) => typeof obj[item] !== "object");
+	return Object.keys(obj).filter(item => typeof obj[item] !== 'object');
 };
 
 const getHeaders = (data: string[]) =>
-  data.map((key) => {
-    let header = key.split(/(?=[A-Z])/).join(" ");
-    return header.charAt(0).toUpperCase() + header.slice(1);
-  });
+	data.map(key => {
+		let header = key.split(/(?=[A-Z])/).join(' ');
+		return header.charAt(0).toUpperCase() + header.slice(1);
+	});
 
 const Table = <T extends {}>({ data = [] }: { data: T[] }) => {
-  let keys = getKeys(data);
-  const headers = getHeaders(keys as string[]);
+	let keys = getKeys(data);
+	const headers = getHeaders(keys as string[]);
 
-  return (
-    <div className="container h-full flex flex-col items-center justify-start border">
-      <h1 className="text-4xl text-white font-bold py-2">Any JSON CRUD</h1>
-      <div
-        style={{
-          gridTemplateColumns: `repeat(${keys.length + 1}, minmax(200px, 1fr))`,
-        }}
-        className="grid text-white w-full border h-full overflow-auto"
-      >
-        {headers.map((header, i) => (
-          <p key={i} className="font-bold border text-center sticky">
-            {header}
-          </p>
-        ))}
+	return (
+		<div className="container h-full flex flex-col items-center justify-start border">
+			<h1 className="text-4xl text-white font-bold py-2">Any JSON CRUD</h1>
+			<div
+				style={{
+					gridTemplateColumns: `repeat(${keys.length + 1}, minmax(200px, 1fr))`,
+				}}
+				className="grid text-white w-full border h-full overflow-auto"
+			>
+				{headers.map((header, i) => (
+					<p key={i} className="font-bold border text-center sticky">
+						{header}
+					</p>
+				))}
 
-        <p className="font-bold border text-center sticky">Action</p>
+				<p className="font-bold border text-center sticky">Action</p>
 
-        {data.map((item, i) => (
-          <Fragment key={i}>
-            {keys.map((key, j) => (
-              <p
-                key={j}
-                className="border text-center px-4 py-2 flex items-center justify-center"
-              >
-                {/* @ts-ignore   */}
-                {item[key]}
-                {/* TODO:fix this */} </p>
-            ))}
-            <p className="border text-center px-4 py-2 flex items-center justify-evenly">
-              <AiFillEdit className="text-2xl" />
-              <AiFillDelete className="text-2xl" />
-            </p>
-          </Fragment>
-        ))}
-      </div>
-    </div>
-  );
+				{data.map((item, i) => (
+					<Fragment key={i}>
+						{keys.map((key, j) => (
+							<p
+								key={j}
+								className="border text-center px-4 py-2 flex items-center justify-center"
+							>
+								{/* @ts-ignore   */}
+								{item[key]}
+								{/* TODO:fix this */}{' '}
+							</p>
+						))}
+						<p className="border text-center px-4 py-2 flex items-center justify-evenly">
+							<AiFillEdit className="text-2xl" />
+							<AiFillDelete className="text-2xl" />
+						</p>
+					</Fragment>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default Table;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,38 @@
-import axios from "axios";
-import { GetServerSideProps } from "next";
-import Head from "next/head";
-import Table from "../components/crud-table";
+import axios from 'axios';
+import { GetServerSideProps } from 'next';
+import Head from 'next/head';
+import Table from '../components/crud-table';
 
 const Home = <T extends {}>({ data = [] }: { data: T[] }) => {
-  return (
-    <div className="flex min-h-screen bg-gray-800 flex-col items-center justify-start py-2">
-      <Head>
-        <title>Any JSON CRUD</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	return (
+		<div className="flex min-h-screen bg-gray-800 flex-col items-center justify-start py-2">
+			<Head>
+				<title>Any JSON CRUD</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 
-      <Table {...{ data }} />
-    </div>
-  );
+			<Table {...{ data }} />
+		</div>
+	);
 };
 
 export default Home;
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  const API_URL = "https://jsonplaceholder.typicode.com";
+	const API_URL = 'https://jsonplaceholder.typicode.com';
 
-  let users = [];
+	let users = [];
 
-  try {
-    const data = await axios.get(`${API_URL}/posts`);
-    users = data.data;
-  } catch (error) {
-    console.log(error);
-  }
+	try {
+		const data = await axios.get(`${API_URL}/posts`);
+		users = data.data;
+	} catch (error) {
+		console.log(error);
+	}
 
-  return {
-    props: {
-      data: JSON.parse(JSON.stringify(users)),
-    },
-  };
+	return {
+		props: {
+			data: JSON.parse(JSON.stringify(users)),
+		},
+	};
 };
